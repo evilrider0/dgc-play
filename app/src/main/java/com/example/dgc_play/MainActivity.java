c7494a1b5600fd2ea5ff2297925ae41487f05acf
Manishankar Vakta <evilrider555@gmail.com>
2020-07-08 03:08:27 +0600

    Initial commit

---
+++ app/src/main/java/com/example/dgc_play/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.dgc_play;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //Create Web View
+        WebView webb = (WebView)findViewById(R.id.webview1);
+        webb.getSettings().setJavaScriptEnabled(true);
+        webb.setWebViewClient(new WebViewClient());
+        webb.loadUrl("http://player.uni-bd.com/");
+
+        //Hook back key with application
+        webb.setOnKeyListener(new View.OnKeyListener()
+        {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event)
+            {
+                if(event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    WebView webView = (WebView) v;
+
+                    switch(keyCode)
+                    {
+                        case KeyEvent.KEYCODE_BACK:
+                            if(webView.canGoBack())
+                            {
+                                webView.goBack();
+                                return true;
+                            }
+                            break;
+                    }
+                }
+
+                return false;
+            }
+        });
+    }
+}
